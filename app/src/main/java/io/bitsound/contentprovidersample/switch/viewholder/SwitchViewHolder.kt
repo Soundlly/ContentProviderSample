49168b2465c102c150e90ec873e1abb0eef23836
pjhjohn <pjhjohn@gmail.com>
2018-03-17 03:05:05 +0900

    Implement Switch Update

--- app/src/main/java/io/bitsound/contentprovidersample/switch/viewholder/SwitchViewHolder.kt
+++ app/src/main/java/io/bitsound/contentprovidersample/switch/viewholder/SwitchViewHolder.kt
@@ -3,6 +3,7 @@ package io.bitsound.contentprovidersample.switch.viewholder
 import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.CompoundButton
 import io.bitsound.contentprovidersample.models.SwitchModel
 import kotlinx.android.synthetic.main.viewholder_switch.view.*
 
@@ -10,9 +11,10 @@ import kotlinx.android.synthetic.main.viewholder_switch.view.*
 class SwitchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     @SuppressLint("SetTextI18n")
-    fun bind(data: SwitchModel, listener: (View) -> Unit) {
+    fun bind(data: SwitchModel, updateListener: (CompoundButton, Boolean) -> Unit, deleteListener: (View) -> Unit) {
         itemView.switch_key.text = "#${data.id ?: "N/A"} : ${data.key}"
         itemView.switch_value.isChecked = data.value
-        itemView.btn_delete_switch.setOnClickListener(listener)
+        itemView.switch_value.setOnCheckedChangeListener(updateListener)
+        itemView.btn_delete_switch.setOnClickListener(deleteListener)
     }
 }
